95a362169a0d0a483c2dbffcbd68effea6b8c495
Alexander Menshikov <n0str@users.noreply.github.com>
2020-01-13 23:19:26 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 from PIL import Image
+from os import listdir
+from os.path import isfile, join
 
 
-def fix_size(fn, desired_w=256, desired_h=256, fill_color=(0, 0, 0, 255)):
+def fix_size(fn, desired_w=256, desired_h=256, fill_color=(0, 0, 0)):
     im = Image.open(fn)
     x, y = im.size
 
@@ -11,10 +13,11 @@ def fix_size(fn, desired_w=256, desired_h=256, fill_color=(0, 0, 0, 255)):
         h = y
         w = int(h * desired_w / desired_h)
 
-    new_im = Image.new('RGBA', (w, h), fill_color)
+    new_im = Image.new('RGB', (w, h), fill_color)
     new_im.paste(im, ((w - x) // 2, (h - y) // 2))
     return new_im.resize((desired_w, desired_h))
 
 
-img = fix_size("5.jpeg", 180, 200, (255, 255, 255, 255))
-img.save("new.png")
+for filename in [f for f in listdir("./images") if isfile(join("./images", f))]:
+    img = fix_size(f"./images/{filename}", 160, 200, (255, 255, 255))
+    img.save(f"./results/{filename}")
